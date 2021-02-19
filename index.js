3ca477870eb932696b2e9f8fa93808be259bb5f4
Darren Tan <tanjzge@gmail.com>
2021-02-19 14:56:52 +1100

    js

---
+++ index.js
@@ -0,0 +1,24 @@
+const carouselRow = document.querySelector('#carousel-container')
+const cardsList = Array.from(document.querySelector('#carousel-container').children);
+const prev = document.querySelector('#prev')
+const next = document.querySelector('#next')
+
+console.log(cardsList)
+console.log(carouselRow)
+const rotateLeft = () => {
+  cardsList.splice(0, 0, cardsList.splice(3, 1)[0])
+  for(let i = 0; i < cardsList.length; ++i) {
+    carouselRow.appendChild(cardsList[i]);
+  }
+}
+
+const rotateRight = () => {
+  cardsList.splice(3, 0, cardsList.splice(0, 1)[0])
+  for(let i = 0; i < cardsList.length; ++i) {
+    carouselRow.appendChild(cardsList[i]);
+  }
+}
+
+next.addEventListener('click', rotateRight)
+prev.addEventListener('click', rotateLeft)
+
