48ae09fc9813378c25764a766bdaa2e19e274aea
Darren Tan <tanjzge@gmail.com>
2021-02-22 15:56:40 +1100

    icons

--- JS/contact.js
+++ JS/contact.js
@@ -1,10 +1,30 @@
 const heading = document.querySelector('#title-contactme')
+const phone = document.querySelector('#phone')
 
-const timeout = () => {
-  setTimeout(
-    function() { heading.innerHTML = "Let me know."} , 2000)
-  setTimeout(
-    function() { heading.innerHTML = "I'll be happy to help!"} , 4000)
+let i1 = 0
+let txt1 = 'Have any more questions?'
+let speed1 = 100
+
+function typeWriter1() {
+  if (i1 < txt1.length) {
+    heading.innerHTML += txt1.charAt(i1);
+    i1++;
+    setTimeout(typeWriter1, speed1);
+  } 
 }
 
-document.addEventListener("DOMContentLoaded", timeout);
+document.addEventListener("DOMContentLoaded", typeWriter1);
+
+let i = 0
+let txt = ' 04 6984 5480'
+let speed = 100
+
+function typeWriter() {
+  if (i < txt.length) {
+    phone.innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+phone.addEventListener('click', typeWriter)
