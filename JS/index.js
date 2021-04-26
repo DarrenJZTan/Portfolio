5d5486d28d4f81e57ca498e2432c59b34959f72b
Darren Tan <tanjzge@gmail.com>
2021-04-26 10:56:02 +1000

    added react skill

--- JS/index.js
+++ JS/index.js
@@ -6,14 +6,14 @@ const next = document.querySelector('#next')
 console.log(cardsList)
 console.log(carouselRow)
 const rotateLeft = () => {
-  cardsList.splice(0, 0, cardsList.splice(3, 1)[0])
+  cardsList.splice(0, 0, cardsList.splice(4, 1)[0])
   for(let i = 0; i < cardsList.length; ++i) {
     carouselRow.appendChild(cardsList[i]);
   }
 }
 
 const rotateRight = () => {
-  cardsList.splice(3, 0, cardsList.splice(0, 1)[0])
+  cardsList.splice(4, 0, cardsList.splice(0, 1)[0])
   for(let i = 0; i < cardsList.length; ++i) {
     carouselRow.appendChild(cardsList[i]);
   }
