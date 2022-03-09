5ad540696a47de78c9f480ca85c60fbb150e4dac
Darren Tan <tanjzge@gmail.com>
2022-03-10 09:31:51 +1100

    fixed some bugs

--- JS/index.js
+++ JS/index.js
@@ -1,15 +1,17 @@
-const carouselRow = document.querySelector('#carousel-container')
+const carouselRow = document.querySelector('#carousel-container');
 const cardsList = Array.from(document.querySelector('#carousel-container').children);
-const prev = document.querySelector('#prev')
-const next = document.querySelector('#next')
+const prev = document.querySelector('#prev');
+const next = document.querySelector('#next');
 
-console.log(cardsList)
-console.log(carouselRow)
+console.log(cardsList);
+console.log(carouselRow);
+
 const rotateLeft = () => {
   cardsList.splice(0, 0, cardsList.splice(4, 1)[0])
   for(let i = 0; i < cardsList.length; ++i) {
-    carouselRow.appendChild(cardsList[i]);op
-}
+    carouselRow.appendChild(cardsList[i]);
+  }
+};
 
 const rotateRight = () => {
   cardsList.splice(4, 0, cardsList.splice(0, 1)[0])
---
+++ education.js
@@ -0,0 +1,36 @@
+const rotateIcon = (event) => {
+  if (event.target.classList.contains("btn")) {
+    const button = event.target.parentElement.parentElement.lastElementChild.firstElementChild.firstElementChild.firstElementChild 
+    if(button.classList.contains("arrow-down")) {
+      button.classList.add("arrow-up")
+      button.classList.remove("arrow-down")
+    } else if (button.classList.contains("arrow-up")) {
+      button.classList.add("arrow-down")
+      button.classList.remove("arrow-up")
+    }
+  }
+  if (event.target.classList.contains("clicking-object")) {
+    const button = event.target.parentElement.parentElement.parentElement.lastElementChild.firstElementChild.firstElementChild.firstElementChild 
+    if(button.classList.contains("arrow-down")) {
+      button.classList.add("arrow-up")
+      button.classList.remove("arrow-down")
+    } else if (button.classList.contains("arrow-up")) {
+      button.classList.add("arrow-down")
+      button.classList.remove("arrow-up")
+    }
+  }
+  if (event.target.classList.contains("arrow-down") || event.target.classList.contains("arrow-up")) {
+    const button = event.target
+    if(button.classList.contains("arrow-down")) {
+      button.classList.add("arrow-up")
+      button.classList.remove("arrow-down")
+    } else if (button.classList.contains("arrow-up")) {
+      button.classList.add("arrow-down")
+      button.classList.remove("arrow-up")
+    }
+  }
+}
+
+
+
+document.querySelector('#container-table').addEventListener('click', rotateIcon)
